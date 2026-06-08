457bb146ed0c81e8f2def1203f4c3272f5e5d4a1
Ruben Lujan <lujanortegaruben@gmail.com>
2026-06-08 14:49:09 -0600

    Cambios 2026-06-08 open comandas 3

--- src/components/ComandasAbiertas.jsx
+++ src/components/ComandasAbiertas.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { postGeneral } from '../services/api';
+import { get } from '../services/api';
 
 function ComandasAbiertas() {
   const [data, setData] = useState([]);
@@ -10,7 +10,7 @@ function ComandasAbiertas() {
     setLoading(true);
     setError('');
     try {
-      const result = await postGeneral('POS/GetOpenComandasSummary');
+      const result = await get('POS/GetOpenComandasSummary');
       if (Array.isArray(result)) {
         setData(result);
       } else {
